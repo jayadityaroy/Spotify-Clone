1500ec2c20b914c173c56d2a42fdea3ba239ad44
jayadityaroy <jayadityaroy09@gmail.com>
2026-07-19 03:02:25 +0530

    added AppUser.java and Playlist.java

---
+++ SB_Backend/spotify-clone/src/main/java/com/joy/spotify_clone/entity/AppUser.java
@@ -0,0 +1,35 @@
+package com.joy.spotify_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "app_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String role;
+    @Column(name = "refresh_token", length = 500)
+    private String refreshToken;
+    @CreationTimestamp // automatically sets the field to the time the entity is first inserted into the database.
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp // automatically updates the field to the current time every time the entity is updated.
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ SB_Backend/spotify-clone/src/main/java/com/joy/spotify_clone/entity/Playlist.java
@@ -0,0 +1,38 @@
+package com.joy.spotify_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "playlist")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Playlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Column(name = "is_public", nullable = false)
+    private Boolean isPublic = false;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @CreationTimestamp // automatically sets the field to the time the entity is first inserted into the database.
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp // automatically updates the field to the current time every time the entity is updated.
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appuser_id", nullable = false)
+    private AppUser appUser;
+}
