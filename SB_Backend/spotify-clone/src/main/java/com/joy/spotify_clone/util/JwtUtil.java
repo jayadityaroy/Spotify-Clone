f23e6697bbe0e83e830d8c1d67f95136034465a8
jayadityaroy <jayadityaroy09@gmail.com>
2026-07-25 03:08:41 +0530

    added JwtUtil.java

---
+++ SB_Backend/spotify-clone/src/main/java/com/joy/spotify_clone/util/JwtUtil.java
@@ -0,0 +1,84 @@
+package com.joy.spotify_clone.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.access.token.expiration}")
+    private Long accessTokenExpiration;
+
+    @Value(("${jwt.refresh.token.expiration}"))
+    private Long refreshTokenExpiration;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(Long id, String name, String email, String role){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", id);
+        claims.put("name", name);
+        claims.put("email", email);
+        claims.put("role", role);
+        claims.put("type", "ACCESS");
+        return createToken(claims, email, accessTokenExpiration);
+    }
+
+    public String generateRefreshToken(Long id, String email){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", id);
+        claims.put("email", email);
+        claims.put("type", "REFRESH");
+        return createToken(claims, email, refreshTokenExpiration);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject, Long accessTokenExpiration) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + accessTokenExpiration);
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+
+}
+
+/*
+// Work of access and refresh token
+Typical flow:
+-> User logs in
+-> Backend returns:
+    -> access token
+    -> refresh token
+-> Frontend uses access token to call APIs
+-> When access token expires:
+    -> frontend sends refresh token
+    -> backend verifies it
+    -> backend issues a new access token
+ */
+
+/*
+A refresh token has a longer expiration time because its job is to keep the user signed in without making them log in again too often.
+ An access token is the token the client sends with most API requests.
+ */
+
+/*
+getSigningKey() -> This converts the secret string into a cryptographic key
+ that can be used to sign and verify JWTs.
+ */
